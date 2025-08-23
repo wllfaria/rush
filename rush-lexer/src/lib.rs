3da4d581201311f501cec1588d153fffe1ea9ce0
Willians Faria <dev.willians.faria@gmail.com>
2025-08-22 23:09:41 -0300

    lexing atoms and pipes

--- rush-lexer/src/lib.rs
+++ rush-lexer/src/lib.rs
@@ -1,18 +1,23 @@
+use std::iter::Peekable;
+use std::str::CharIndices;
+
+use token::{Token, TokenKind};
+
 use crate::token::TokenStream;
 
 mod token;
 
 #[derive(Debug)]
 pub struct Lexer<'src> {
-    needle: &'src str,
     source: &'src str,
+    chars: Peekable<CharIndices<'src>>,
 }
 
 impl<'src> Lexer<'src> {
-    pub fn new(text: &'src str) -> Self {
+    pub fn new(source: &'src str) -> Self {
         Self {
-            needle: text,
-            source: text,
+            source,
+            chars: source.char_indices().peekable(),
         }
     }
 
@@ -22,13 +27,94 @@ impl<'src> Lexer<'src> {
             return TokenStream::new(tokens);
         }
 
-        loop {
-            let mut chars = self.needle.chars().peekable();
-            let curr = chars.next().expect("next character should always be available");
-            let next = chars.peek();
-            todo!()
+        while let Some((byte_pos, curr)) = self.next() {
+            let next = self.peek();
+
+            match (curr, next) {
+                ('|', _) => tokens.push(TokenKind::Pipe.into_token(byte_pos)),
+                (';', _) => tokens.push(TokenKind::Semi.into_token(byte_pos)),
+                _ => tokens.push(self.take_atom(byte_pos)),
+            }
         }
 
+        tokens.push(self.eof());
         TokenStream::new(tokens)
+    }
+
+    fn take_atom(&mut self, start: usize) -> Token {
+        let end = self.take_while(|c| !is_delimiter(c));
+        TokenKind::Atom.into_token((start, end))
+    }
+
+    fn next(&mut self) -> Option<(usize, char)> {
+        self.chars.next()
+    }
+
+    fn peek(&mut self) -> Option<(usize, char)> {
+        self.chars.peek().copied()
+    }
+
+    fn eof(&self) -> Token {
+        TokenKind::Eof.into_token(self.source.len())
+    }
+
+    fn take_while<F>(&mut self, f: F) -> usize
+    where
+        F: Fn(char) -> bool,
+    {
+        let mut last = None;
+        while let Some((byte_pos, ch)) = self.peek() {
+            if !f(ch) {
+                break;
+            };
+
+            last = Some((byte_pos, ch));
+            self.next();
+        }
+
+        match last {
+            Some((i, ch)) => i + ch.len_utf8(),
+            None => self.source.len(),
+        }
+    }
+}
+
+#[inline]
+fn is_space(ch: char) -> bool {
+    matches!(ch, ' ' | '\t' | '\n' | '\r')
+}
+
+fn is_delimiter(ch: char) -> bool {
+    is_space(ch) || matches!(ch, '|' | ';')
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::token::BytePos;
+
+    trait TokenSource<'src> {
+        fn token_source(&self, source: &'src str) -> &'src str;
+    }
+
+    impl<'src> TokenSource<'src> for (BytePos, BytePos) {
+        fn token_source(&self, source: &'src str) -> &'src str {
+            &source[*self.0..*self.1]
+        }
+    }
+
+    #[derive(Debug)]
+    struct TokenTest<'src>(TokenKind, &'src str);
+
+    #[test]
+    fn test_lexing_atom() {
+        let source = "some-command-here";
+        let tokens = Lexer::new(source)
+            .lex()
+            .into_iter()
+            .map(|t| TokenTest(t.0, t.1.token_source(source)))
+            .collect::<Vec<_>>();
+
+        panic!("{tokens:#?}")
     }
 }
--- rush-lexer/src/token.rs
+++ rush-lexer/src/token.rs
@@ -1,32 +1,80 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct BytePos(usize);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Token(TokenKind, BytePos);
+impl std::ops::Deref for BytePos {
+    type Target = usize;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<usize> for BytePos {
+    fn from(value: usize) -> Self {
+        Self(value)
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum Value {
-    Atom(BytePos, BytePos),
-}
+pub struct Token(pub(crate) TokenKind, pub(crate) (BytePos, BytePos));
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum TokenKind {
-    Value(Value),
+    Atom,
+    Pipe,
+    Semi,
+    Eof,
+}
+
+pub trait IntoBytePos {
+    fn into_byte_pos(self) -> (BytePos, BytePos);
+}
+
+impl IntoBytePos for (usize, usize) {
+    fn into_byte_pos(self) -> (BytePos, BytePos) {
+        (self.0.into(), self.1.into())
+    }
+}
+
+impl IntoBytePos for usize {
+    fn into_byte_pos(self) -> (BytePos, BytePos) {
+        (self.into(), self.into())
+    }
 }
 
 impl TokenKind {
-    pub fn into_token(self, byte_pos: impl Into<BytePos>) -> Token {
-        Token(self, byte_pos.into())
+    pub fn into_token(self, position: impl IntoBytePos) -> Token {
+        Token(self, position.into_byte_pos())
     }
 }
 
 #[derive(Debug)]
 pub struct TokenStream {
     tokens: Vec<Token>,
+    cursor: usize,
 }
 
 impl TokenStream {
     pub fn new(tokens: Vec<Token>) -> Self {
-        Self { tokens }
+        Self { tokens, cursor: 0 }
+    }
+
+    pub fn next(&mut self) -> TokenKind {
+        match self.tokens.get(self.cursor).copied() {
+            Some(token) => {
+                self.cursor += 1;
+                token.0
+            }
+            None => TokenKind::Eof,
+        }
+    }
+}
+
+impl IntoIterator for TokenStream {
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+    type Item = Token;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tokens.into_iter()
     }
 }
